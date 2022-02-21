98271afb1c6056db27ef9bbc66820312da83916f
bsord <brandon.sorgdrager@gmail.com>
2022-02-21 14:29:19 -0600

    Fix timeAgo locale redundant calls

--- src/Util/FormattedTime.js
+++ src/Util/FormattedTime.js
@@ -1,8 +1,7 @@
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
-
+TimeAgo.addDefaultLocale(en)
 export const timeAgoShort = (timestamp) =>{
-  TimeAgo.addDefaultLocale(en)
-  const timeAgo = new TimeAgo('en-US')
+  const timeAgo = new TimeAgo()
   return timeAgo.format(new Date(timestamp*1000), 'twitter')
 }
