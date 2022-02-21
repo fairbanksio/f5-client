332feddedb05d6ba46cbbed2d37785d5e71c8738
bsord <brandon.sorgdrager@gmail.com>
2022-02-21 17:25:44 -0600

    Fixed header, added footer, added emojies

--- src/App.js
+++ src/App.js
@@ -2,30 +2,31 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Grid,
-  GridItem,
+  Flex
 } from '@chakra-ui/react';
 import PostView from './Components/PostView';
 import { RefreshIntervalProvider} from './Contexts/RefreshIntervalContext'
 import { SubredditProvider} from './Contexts/SubredditContext'
-import Header from './Components/Header'
 import CustomTheme from './Themes/CustomTheme'
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
     <ChakraProvider theme={CustomTheme}>
       <RefreshIntervalProvider>
         <SubredditProvider>
-          <Box >
-            <Grid p={3} gap={3} >
-              <GridItem >
-                <Header/>
-              </GridItem>
-              <GridItem >
-                <PostView/>
-              </GridItem>
-            </Grid>
-          </Box>
+          <Flex minHeight='100vh' direction='column' p={0}>
+            <Box>
+              <Navbar/>
+            </Box>
+            <Box flex='1'>
+              <PostView/>
+            </Box>
+            <Box >
+              <Footer/>
+            </Box>
+          </Flex>
         </SubredditProvider>
       </RefreshIntervalProvider>
     </ChakraProvider>
---
+++ src/Components/Footer.js
@@ -0,0 +1,34 @@
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  IconButton,
+  Link,
+  Button,
+  Icon
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaYoutube,FaGithub } from 'react-icons/fa';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+
+export default function Footer() {
+  return (
+
+      <Container
+        as={Stack}
+        maxW='container.xl'
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>Maintained with &#10084; by <Link href="https://github.com/bsord" isExternal>bsord</Link> and <Link href="https://github.com/jonfairbanks" isExternal>jonfairbanks</Link></Text>
+        <Stack direction={'row'} spacing={6}>
+          <Link href='https://github.com/Fairbanks-io/f5-client/issues/new' isExternal>
+            Request a subreddit <Icon as={FaGithub} mx='2px' />
+          </Link>
+        </Stack>
+      </Container>
+  );
+}
